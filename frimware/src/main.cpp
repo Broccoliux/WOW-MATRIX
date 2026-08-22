59b6a42fab53647f77189187b3c8d0c1dd1e1985
Nabeel Ahmed <fussdeek@gmail.com>
2026-08-22 11:13:05 +0500

    Update main.cpp

--- frimware/src/main.cpp
+++ frimware/src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
+#include "cube.h"
+#include "wifi.h"
 
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("ESP32 dev board is alive and connected!");
+  Serial.println("ESP32 Dev Board Starting Up...");
+
+  cube_init();
+
+  wifi_init_ap();
 }
 
 void loop()
