539919d9e174f33d0d19beac2373b215ac513823
Nabeel Ahmed <fussdeek@gmail.com>
2026-08-22 11:13:30 +0500

    Update wifi.cpp

--- frimware/src/wifi.cpp
+++ frimware/src/wifi.cpp
@@ -1,6 +1,8 @@
-#include <Arduino.h>
+#include <WiFiType.h>
 #include <WiFi.h>
 #include "wifi.h"
+#include <Arduino.h>
+
 
 const char *ssid = "LED_Cube_Controller";
 const char *password = "password123";
@@ -9,10 +11,10 @@ void wifi_init_ap()
 {
   Serial.println("Setting up Access Point...");
 
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
 
-  ::WiFi.softAP(ssid, password);
-
-  IPAddress myIP = ::WiFi.softAPIP();
+  IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
 }
