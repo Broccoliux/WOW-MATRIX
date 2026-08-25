64b2953a201cc5ce1b1e755ec4fcd7ad8ceb2d3d
Nabeel Ahmed <fussdeek@gmail.com>
2026-08-25 13:39:09 +0500

    Update wifi.cpp

--- frimware/src/wifi.cpp
+++ frimware/src/wifi.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <WiFiType.h>
 #include <WiFi.h>
-#include "wifi.h"
+#include "wifi_controller.h"
 
 const char *ssid = "LED_Cube_Controller";
 const char *password = "password123";
